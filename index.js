9646d8d235b854711d52434c1ad308f2e53fe30d
Spina4ko <dimitargeorgievpavlov@gmail.com>
2023-06-14 11:43:04 +0300

    ComitAndPush

---
+++ index.js
@@ -0,0 +1,5 @@
+let button = document.querySelector(".searchbutton");
+button.addEventListener("click", () => {
+    let input = document.querySelector(".searchbar");
+    console.log(input.value);
+});
